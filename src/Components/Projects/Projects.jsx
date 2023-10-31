e05f618b4f8cd73fdcb562d4f48ed248a1a46a83
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2023-10-31 22:00:47 +0600

    projects complete

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Project.css'
 import electromart from '../../Images/Electromart.png';
+import Aplusacedemy from '../../Images/Aplus.png';
 import { FiGithub } from "react-icons/fi";
 import { BiLinkExternal } from "react-icons/bi";
 import { IoServer } from "react-icons/io5";
@@ -8,8 +9,8 @@ import { IoServer } from "react-icons/io5";
 
 const Projects = () => {
     return (
-        <div className='mx-32'>
-            <h1 className='text-orange-500 text-center text-3xl'>Projects</h1>
+        <div className='project-container mx-0 lg:mx-32'>
+            <h1 className='text-orange-500 text-center text-3xl mb-8'>Projects</h1>
             <div className='projects'>
             <div className="max-w-sm  overflow-hidden p-4 shadow-2xl hover:-translate-y-2 duration-300">
                 <img className="w-full rounded-xl" src={electromart} alt="Sunset in the mountains"/>
@@ -23,14 +24,16 @@ const Projects = () => {
 
                 <div className="px-6 py-0">
                     <div className="font-bold text-orange-500 text-xl mb-2">Electro Mart</div>
-                    <p className="text-white text-base">
-                    An E-commerce website to sell and buy products.
-                    Buyer can browse products at once or categorically.
-                    Search filter, pagination applied.
-                    Add products to the cart and remove from the cart, login required to place order.
-                    Seller can list their products by logging in to a protected route.
-                    Update, delete product from seller dashboard.
-                    </p>
+                    <div className="text-white text-base">
+                    <p className='text-lg text-neutral-400 font-medium mb-4'> A E-commerce website to sell and buy products.</p>
+                    <ul className='list-disc'>
+                        <li>Buyer can browse products at once or categorically.</li>
+                        <li>Search filter, pagination applied</li>
+                        <li>Add products to the cart and remove from the cart, login required to place order</li>
+                        <li> Seller can list their products by logging in to a protected route</li>
+                        <li>Update, delete product from seller dashboard</li>
+                    </ul>
+                    </div>
                     <div className='mt-2 flex gap-x-12'>
                         <a href="https://electromart-ccd61.web.app/">
                              <span><BiLinkExternal  className='text-white text-xl hover:text-orange-500' alt='Live'/></span>
@@ -49,33 +52,33 @@ const Projects = () => {
             </div>
 
             <div className="max-w-sm  overflow-hidden p-4 shadow-2xl hover:-translate-y-2 duration-300">
-                <img className="w-full rounded-xl" src={electromart} alt="Sunset in the mountains"/>
+                <img className="w-full rounded-xl" src={Aplusacedemy} alt="Sunset in the mountains"/>
                 <div class="px-6 pt-2 pb-2">
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">ReactJs</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Tailwind</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Firebase</span>
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">ExpressJs</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">MongoDB</span>
                 </div>
 
                 <div className="px-6 py-0">
-                    <div className="font-bold text-orange-500 text-xl mb-2">Electro Mart</div>
-                    <p className="text-white text-base">
-                    An E-commerce website to sell and buy products.
-                    Buyer can browse products at once or categorically.
-                    Search filter, pagination applied.
-                    Add products to the cart and remove from the cart, login required to place order.
-                    Seller can list their products by logging in to a protected route.
-                    Update, delete product from seller dashboard.
-                    </p>
+                    <div className="font-bold text-orange-500 text-xl mb-2">A Plus Academy</div>
+                    <div className="text-white text-base">
+                    <p className='text-lg text-neutral-400 font-medium mb-4'> An Online learning and teaching platform</p>
+                    <ul className='list-disc'>
+                    <li>Modern looking Website</li>
+                    <li>User can browse courses by log in</li>
+                    <li>Show user reviews</li>
+                    <li>Send feedback to admin</li>
+                    </ul>
+                    </div>
                     <div className='mt-2 flex gap-x-12'>
-                        <a href="https://electromart-ccd61.web.app/">
+                        <a href="https://aplusacademy-rabbi.web.app/">
                              <span><BiLinkExternal  className='text-white text-xl hover:text-orange-500' alt='Live'/></span>
                          </a>
-                        <a href="https://github.com/GRabbi467/ElectroMart.git">
+                        <a href="https://github.com/GRabbi467/AplusAcademy.git">
                              <span><FiGithub  className='text-white text-xl hover:text-orange-500' alt='Live'/></span>
                          </a>
-                         <a href="https://github.com/GRabbi467/ElectroMart-server.git">
+                         <a href="https://github.com/GRabbi467/Edusite-Server.git">
                              <span><IoServer  className='text-white text-xl hover:text-orange-500' alt='Live'/></span>
                          </a>
                          
