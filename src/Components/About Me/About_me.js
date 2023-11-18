a0c1ce28a819f105a862f8df1c6e8bb0274bc055
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2023-11-18 20:33:44 +0600

    email me

--- src/Components/About Me/About_me.js
+++ src/Components/About Me/About_me.js
@@ -12,7 +12,7 @@ const About_me = () => {
              <a href="mailto:g.rabbidiu@gmail.com" 
              className="px-10 py-4 text-xl font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500 md:w-auto  
              ">
-              Email Me
+             Contact Me by Email
              </a>
              </div>
         </div>
