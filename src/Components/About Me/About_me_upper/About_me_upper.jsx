3fd00b51e8beabacec9fde955d45f2f66978cc95
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2023-11-18 19:52:10 +0600

    home fixed

--- src/Components/About Me/About_me_upper/About_me_upper.jsx
+++ src/Components/About Me/About_me_upper/About_me_upper.jsx
@@ -6,7 +6,7 @@ const About_me_upper = () => {
         <div className='about-upper'>  
             <div className="sec">
             <div className='about_section'>
-             <disv className='about_me_div'>
+             <div className='about_me_div'>
                 <div className='about_img'>
                    <div> <img  src={Rabbi1} alt="" /></div>
                 </div>
@@ -14,7 +14,7 @@ const About_me_upper = () => {
                 <p className='text-lg md:text-2xl text-white'>I'm a highly motivated and enthusiastic Junior Full Stack Developer with a strong passion for technology and a desire to make a meaningful impact in the world of web development.</p>
                 <p className=' text-lg md:text-2xl text-white'>With a background in computer science and a recent completion of a coding bootcamp, I am eager to kickstart my career and contribute to exciting projects.</p>
                 </div>
-             </disv>
+             </div>
              </div>
             </div>
         </div>
